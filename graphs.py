bd5d7b1c9be360fc0520a4e523fb9b8046040f39
gitjcb <151371984+gitjcb@users.noreply.github.com>
2024-02-17 19:59:25 +0000

    Gets day data from json file

--- graphs.py
+++ graphs.py
@@ -3,10 +3,11 @@ import json
 import matplotlib.pyplot as plt
 
 def plotting(jsonFile):
+    high_values = []
+    time_values = []
     # Load the JSON data from the file
     with open(jsonFile) as f:
         file = json.load(f)
-
 
     ## adapt to json input:
     ## for time, values in jsonFile['Time Series (5min)].items():
@@ -14,8 +15,7 @@ def plotting(jsonFile):
 
 
     # Extract high values and their corresponding time values
-    high_values = []
-    time_values = []
+        
     for time_value, values in file['Time Series (5min)'].items():
         high_values.append(float(values['2. high']))
         time_values.append(pd.to_datetime(time_value))
@@ -35,4 +35,18 @@ def plotting(jsonFile):
 
     return df
 
-plotting("resources/2023-01_hourly_AAPL.json")
+def get_day_data(jsonfile, shares_owned):
+    with open(jsonfile) as f:
+        file = json.load(f)
+        company = file['Meta Data']['2. Symbol']
+        all_values = list(file['Time Series (5min)'].values()) # Get all values at each 5-minute interval as a list
+        price_start = float(all_values[-1]['1. open'])
+        price_end = float(all_values[0]['4. close'])
+        change = price_end - price_start
+        equity = shares_owned * price_end
+        return_today = shares_owned * change
+    return company, price_start, change, equity, return_today
+
+shares_owned = 123
+plotting('resources/2023-01_hourly_AAPL.json')
+print(get_day_data('resources/2023-01_hourly_AAPL.json', shares_owned))

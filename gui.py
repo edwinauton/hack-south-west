4ea549e8e9311331ebcc6f339262c46ab4b4f911
edwinauton <ea994@bath.ac.uk>
2024-02-17 21:29:20 +0000

    Combine files and iterate overall all json files

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
-import random
+import os
 import sys
+from share import Share
 
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import Qt
@@ -8,15 +9,29 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
 def create_button(text):
+	"""Creates a button with the given text"""
 	button = QPushButton()
 	button.setText(text)
 	return button
 
 
 def create_label(text):
+	"""Creates a label with the given text and centres it"""
 	label = QLabel(text)
 	label.setAlignment(Qt.AlignCenter)
 	return label
+
+
+def get_shares_list():
+	shares_list = list()
+
+	for subdir, dirs, files in os.walk("resources"):
+		for file in files:
+			filepath = subdir + os.sep + file
+			if filepath.endswith(".json"):
+				shares_list.append(Share(filepath, 10))
+
+	return shares_list
 
 
 class Window(QDialog):
@@ -52,33 +67,38 @@ class Window(QDialog):
 		header.addWidget(spacer)
 		header.addWidget(spacer)
 
+		shares_owned = create_label("Shares Owned")
+		header.addWidget(shares_owned)
+
 		self.table.addLayout(header)
 
 		# Add spacing after heading
 		self.table.insertSpacing(1, 25)
 
-		for i in range(1, 20):
+		shares = get_shares_list()
+
+		for share in shares:
 			# Create row
 			row = QHBoxLayout()
 
 			# Ticker Button
-			ticker = create_button("[Ticker]")
+			ticker = create_button(str(share.ticker))
 			row.addWidget(ticker)
 
 			# Last Price
-			last_price = create_label("[Last Price]")
+			last_price = create_label(str(share.start_price))
 			row.addWidget(last_price)
 
 			# Change
-			change = create_label("[Change]")
+			change = create_label(str(share.change))
 			row.addWidget(change)
 
 			# Equity
-			equity = create_label("[Equity]")
+			equity = create_label(str(share.equity))
 			row.addWidget(equity)
 
 			# Daily Return
-			daily_return = create_label("[Return]")
+			daily_return = create_label(str(share.daily_return))
 			row.addWidget(daily_return)
 
 			# Buy Button
@@ -89,17 +109,21 @@ class Window(QDialog):
 			sell_button = create_button("Sell")
 			row.addWidget(sell_button)
 
-			# Add row to vbox (table)
+			# Share Number
+			shares_owned = create_label(str(share.number_owned))
+			row.addWidget(shares_owned)
+
+			# Add row to table
 			self.table.addLayout(row)
 
 		# Setup Grid
 		layout = QGridLayout()
 
 		# Titles
-		profile_value = create_label("Profile Value: [N/A]")
+		profile_value = create_label(f"Profile Value: {round(sum(share.equity for share in shares), 2)}")
 		layout.addWidget(profile_value, 0, 1, 1, 1)
 
-		daily_gain = create_label("Today's Gain: [N/A]")
+		daily_gain = create_label(f"Today's Return: {round(sum(share.daily_return for share in shares), 2)}")
 		layout.addWidget(daily_gain, 1, 1, 1, 1)
 
 		# Scrollable Table
@@ -110,7 +134,7 @@ class Window(QDialog):
 
 		# Graph
 		layout.addWidget(self.canvas, 2, 0, 1, 3)
-		self.plot([random.random() for _ in range(50)])
+		self.plot(shares[0])
 
 		# Window Properties
 		self.setWindowTitle('Stock Trading Simulator')
@@ -118,11 +142,12 @@ class Window(QDialog):
 		self.setLayout(layout)
 		self.show()
 
-	def plot(self, data):
+	def plot(self, share):
 		"""Plots the data given in an array on the graph shown in the window"""
 		self.figure.clear()
 		graph = self.figure.add_subplot(111)
-		graph.plot(data, '*-')
+		data = share.create_graph()
+		graph.plot(data[0], data[1],  marker='.', linestyle='-')
 		self.canvas.draw()
 
 
---
+++ share.py
@@ -0,0 +1,47 @@
+import json
+
+import pandas as pd
+
+
+class Share:
+	def __init__(self, json_file, shares_owned):
+		self.json_file = json_file
+		self.number_owned = shares_owned
+		self.ticker = self.process_json()[0]
+		self.start_price = self.process_json()[1]
+		self.change = self.process_json()[2]
+		self.equity = self.process_json()[3]
+		self.daily_return = self.process_json()[4]
+
+	def process_json(self):
+		"""Parse the given json file and return the required values as a tuple"""
+		with open(self.json_file) as f:
+			file = json.load(f)
+			ticker = file['Meta Data']['2. Symbol']
+			all_values = list(file['Time Series (5min)'].values())
+			start_price = float(all_values[-1]['1. open'])
+			end_price = float(all_values[0]['4. close'])
+			change = end_price - start_price
+			equity = self.number_owned * end_price
+			daily_return = self.number_owned * change
+		return ticker, round(start_price, 2), round(change, 2), round(equity, 2), round(daily_return, 2)
+
+	def create_graph(self):
+		"""Return x and y values for the given file"""
+		high_values = []
+		time_values = []
+
+		# Load the JSON data from the file
+		with open(self.json_file) as f:
+			file = json.load(f)
+
+		# Extract high values and their corresponding time values
+		for time_value, values in file['Time Series (5min)'].items():
+			high_values.append(float(values['2. high']))
+			time_values.append(pd.to_datetime(time_value))
+
+		# Create DataFrame
+		df = pd.DataFrame({'Time': time_values, 'High': high_values})
+
+		# Return the x and y values
+		return df['Time'], df['High']

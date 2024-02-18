371e8fb9247666c5eb43756cb3d3709c9463db53
edwinauton <ea994@bath.ac.uk>
2024-02-18 01:22:38 +0000

    Vertically lock labels

--- gui.py
+++ gui.py
@@ -5,7 +5,8 @@ from functools import partial
 
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QLabel, QGridLayout, QScrollArea, QWidget, QHBoxLayout, QVBoxLayout, QPushButton)
+from PyQt5.QtWidgets import (QApplication, QLabel, QGridLayout, QScrollArea, QWidget, QHBoxLayout, QVBoxLayout,
+							 QPushButton, QSizePolicy)
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 from share import Share
@@ -22,6 +23,7 @@ def create_label(text):
 	"""Creates a label with the given text and centres it"""
 	label = QLabel(text)
 	label.setAlignment(Qt.AlignCenter)
+	label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 	return label
 
 
@@ -52,12 +54,12 @@ class Window(QWidget):
 		self.widget = QWidget()
 		self.table = QVBoxLayout()
 		self.profile_value = QLabel()
-		self.daily_return = QLabel()
+		self.overall_return = QLabel()
 		self.ticker = QPushButton()
 		self.last_price = QLabel()
 		self.change = QLabel()
 		self.equity = QLabel()
-		self.todays_return = QLabel()
+		self.daily_return = QLabel()
 		self.buy_button = QPushButton()
 		self.sell_button = QPushButton()
 		self.shares_owned = QLabel()
@@ -76,10 +78,10 @@ class Window(QWidget):
 		change = create_label("Change")
 		header.addWidget(change)
 
-		equity = create_label("Your Equity")
+		equity = create_label("Equity")
 		header.addWidget(equity)
 
-		daily_return = create_label("Today's Return")
+		daily_return = create_label("Return")
 		header.addWidget(daily_return)
 
 		spacer = create_label("")
@@ -118,8 +120,8 @@ class Window(QWidget):
 			row.addWidget(self.equity)
 
 			# Daily Return
-			self.todays_return = create_label(str(share.daily_return))
-			row.addWidget(self.todays_return)
+			self.daily_return = create_label(str(share.daily_return))
+			row.addWidget(self.daily_return)
 
 			self.shares_owned = create_label(str(share.number_owned))
 
@@ -143,8 +145,8 @@ class Window(QWidget):
 		# Titles
 		self.profile_value = create_label(f"Profile Value: {round(sum(share.equity for share in shares), 2)}")
 		layout.addWidget(self.profile_value, 0, 1, 1, 1)
-		self.daily_return = create_label(f"Today's Return: {round(sum(share.daily_return for share in shares), 2)}")
-		layout.addWidget(self.daily_return, 1, 1, 1, 1)
+		self.overall_return = create_label(f"Today's Return: {round(sum(share.daily_return for share in shares), 2)}")
+		layout.addWidget(self.overall_return, 1, 1, 1, 1)
 
 		# Scrollable Table
 		self.widget.setLayout(self.table)

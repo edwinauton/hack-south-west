76b57c84cd751fa1e256a7916290c6de9ea771b5
edwinauton <ea994@bath.ac.uk>
2024-02-18 04:49:56 +0000

    Formatting
    And a few bug fixes

--- utils.py
+++ utils.py
@@ -4,70 +4,72 @@ import os
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QPushButton, QLabel, QSizePolicy, QFrame
 
+from stock import Stock
+
 
 # Function to create a button with the given text
 def create_button(text):
-	button = QPushButton()
-	button.setText(text)
-	return button
+    button = QPushButton()
+    button.setText(text)
+    return button
 
 
 # Function to create a label with the given text
 def create_label(text):
-	label = QLabel(text)
-	label.setAlignment(Qt.AlignCenter)
-	label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-	return label
+    label = QLabel(text)
+    label.setAlignment(Qt.AlignCenter)
+    label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+    return label
 
 
 # Function to create a dividing horizontal line
 def create_line():
-	line = QFrame()
-	line.setFrameShape(QFrame.HLine)
-	line.setObjectName("line")
-	return line
+    line = QFrame()
+    line.setFrameShape(QFrame.HLine)
+    line.setObjectName("line")
+    return line
 
 
 # Function to add a $ sign in front of a value
 def format(data):
-	string = str(data)
-	if string[0] == "-":
-		return "-$" + string[1:]
-	else:
-		return "$" + string
+    string = str(data)
+    if string[0] == "-":
+        return "-$" + string[1:]
+    else:
+        return "$" + string
 
 
 # Function to colour regular loss/gain figures
 def colour(label):
-	if "-" in label.text():
-		label.setObjectName("loss")
-	else:
-		label.setObjectName("gain")
+    if "-" in label.text():
+        label.setObjectName("loss")
+    else:
+        label.setObjectName("gain")
 
 
 # Function to colour main loss/gain figure
 def colour_heading(heading):
-	if "-" in heading.text():
-		heading.setObjectName("heading_loss")
-	else:
-		heading.setObjectName("heading_gain")
+    if "-" in heading.text():
+        heading.setObjectName("heading_loss")
+    else:
+        heading.setObjectName("heading_gain")
 
 
 # Function to create a list of stocks from a file
 def get_stocks_list():
-	# Read stocks stored in file
-	with open("stock_record.json") as f:
-		data = json.load(f)
+    # Read stocks stored in file
+    with open("stock_record.json") as f:
+        data = json.load(f)
 
-	stocks_list = []
+    stocks_list = []
 
-	# Read data from files provided by API
-	for subdir, dirs, files in os.walk("resources"):
-		for file in files:
-			filepath = subdir + os.sep + file
-			if file.endswith(".json"):
-				filename = os.path.splitext(file)[0]
-				number_of_stocks = data[filename]
-				stocks_list.append(Stock(filepath, number_of_stocks))
+    # Read data from files provided by API
+    for subdir, dirs, files in os.walk("resources"):
+        for file in files:
+            filepath = subdir + os.sep + file
+            if file.endswith(".json"):
+                filename = os.path.splitext(file)[0]
+                number_of_stocks = data[filename]
+                stocks_list.append(Stock(filepath, number_of_stocks))
 
-	return stocks_list
+    return stocks_list
--- window.py
+++ window.py
@@ -1,204 +1,209 @@
-import json
 import sys
 from functools import partial
 
 import matplotlib.pyplot as plt
 import mplcursors
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import (QApplication, QLabel, QGridLayout, QScrollArea, QWidget, QHBoxLayout, QVBoxLayout,
-							 QPushButton, QFrame)
+from PyQt5.QtWidgets import QApplication, QLabel, QGridLayout, QScrollArea, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QFrame
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 import utils
 
 
 class Window(QWidget):
-	def __init__(self, parent=None):
-		super(Window, self).__init__(parent)
-		self.figure = plt.figure()
-		self.canvas = FigureCanvas(self.figure)
-		self.scroll = QScrollArea()
-		self.widget = QWidget()
-		self.table = QVBoxLayout()
-		self.portfolio_value = QLabel()
-		self.overall_return = QLabel()
-		self.ticker = QPushButton()
-		self.last_price = QLabel()
-		self.change = QLabel()
-		self.equity = QLabel()
-		self.daily_return = QLabel()
-		self.buy_button = QPushButton()
-		self.sell_button = QPushButton()
-		self.stocks_owned = QLabel()
-		self.init()
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.scroll = QScrollArea()
+        self.widget = QWidget()
+        self.table = QVBoxLayout()
+        self.portfolio_value = QLabel()
+        self.overall_return = QLabel()
+        self.ticker = QPushButton()
+        self.last_price = QLabel()
+        self.change = QLabel()
+        self.equity = QLabel()
+        self.daily_return = QLabel()
+        self.buy_button = QPushButton()
+        self.sell_button = QPushButton()
+        self.stocks_owned = QLabel()
+        self.stocks = list()
+        self.init()
 
-	def init(self):
-		# Table Header
-		header = QHBoxLayout()
+    def init(self):
+        # Table Header
+        header = QHBoxLayout()
 
-		spacer = utils.create_label("")
-		header.addWidget(spacer)
+        spacer = utils.create_label("")
+        header.addWidget(spacer)
 
-		price = utils.create_label("Price")
-		price.setObjectName("subheading")
-		header.addWidget(price)
+        price = utils.create_label("Price")
+        price.setObjectName("subheading")
+        header.addWidget(price)
 
-		change = utils.create_label("Change")
-		change.setObjectName("subheading")
-		header.addWidget(change)
+        change = utils.create_label("Change")
+        change.setObjectName("subheading")
+        header.addWidget(change)
 
-		equity = utils.create_label("Equity")
-		equity.setObjectName("subheading")
-		header.addWidget(equity)
+        equity = utils.create_label("Equity")
+        equity.setObjectName("subheading")
+        header.addWidget(equity)
 
-		daily_return = utils.create_label("Return")
-		daily_return.setObjectName("subheading")
-		header.addWidget(daily_return)
+        daily_return = utils.create_label("Return")
+        daily_return.setObjectName("subheading")
+        header.addWidget(daily_return)
 
-		header.addWidget(spacer)
-		header.addWidget(spacer)
+        header.addWidget(spacer)
+        header.addWidget(spacer)
 
-		stocks_owned = utils.create_label("Stocks Owned")
-		stocks_owned.setObjectName("subheading")
-		header.addWidget(stocks_owned)
+        stocks_owned = utils.create_label("Stocks Owned")
+        stocks_owned.setObjectName("subheading")
+        header.addWidget(stocks_owned)
 
-		self.table.addLayout(header)
-		self.table.addWidget(utils.create_line())
+        self.table.addLayout(header)
+        self.table.addWidget(utils.create_line())
 
-		# Add spacing after heading
-		self.table.insertSpacing(1, 10)
-		self.table.addWidget(utils.create_line())
+        # Add spacing after heading
+        self.table.insertSpacing(1, 10)
+        self.table.addWidget(utils.create_line())
 
-		stocks = utils.get_stocks_list()
+        self.stocks = utils.get_stocks_list()
 
-		for stock in stocks:
-			# Setup Row
-			row = QHBoxLayout()
+        for stock in self.stocks:
+            # Setup Row
+            row = QHBoxLayout()
 
-			# Ticker Button
-			self.ticker = utils.create_button(str(stock.ticker))
-			self.ticker.clicked.connect(partial(self.update_graph, stock))
-			row.addWidget(self.ticker)
+            # Ticker Button
+            self.ticker = utils.create_button(str(stock.ticker))
+            self.ticker.clicked.connect(partial(self.update_graph, stock))
+            row.addWidget(self.ticker)
 
-			# Last Price
-			value = format(stock.start_price)
-			self.last_price = utils.create_label(value)
-			row.addWidget(self.last_price)
+            # Last Price
+            value = utils.format(stock.start_price)
+            self.last_price = utils.create_label(value)
+            row.addWidget(self.last_price)
 
-			# Change
-			value = format(stock.change)
-			self.change = utils.create_label(value)
-			utils.colour(self.change)
-			row.addWidget(self.change)
+            # Change
+            value = utils.format(stock.change)
+            self.change = utils.create_label(value)
+            utils.colour(self.change)
+            row.addWidget(self.change)
 
-			# Equity
-			value = format(stock.equity)
-			self.equity = utils.create_label(value)
-			row.addWidget(self.equity)
+            # Equity
+            value = utils.format(stock.equity)
+            self.equity = utils.create_label(value)
+            row.addWidget(self.equity)
 
-			# Daily Return
-			value = format(stock.daily_return)
-			self.daily_return = utils.create_label(value)
-			utils.colour(self.daily_return)
-			row.addWidget(self.daily_return)
+            # Daily Return
+            value = utils.format(stock.daily_return)
+            self.daily_return = utils.create_label(value)
+            utils.colour(self.daily_return)
+            row.addWidget(self.daily_return)
 
-			self.stocks_owned = utils.create_label(str(stock.number_owned))
+            self.stocks_owned = utils.create_label(str(stock.number_owned))
 
-			# Buy Button
-			self.buy_button = utils.create_button("Buy")
-			self.buy_button.clicked.connect(partial(self.update_stocks, 1, stock))
-			row.addWidget(self.buy_button)
+            # Buy Button
+            self.buy_button = utils.create_button("Buy")
+            self.buy_button.clicked.connect(partial(self.update_stocks, 1, stock))
+            row.addWidget(self.buy_button)
 
-			# Sell Button
-			self.sell_button = utils.create_button("Sell")
-			self.sell_button.clicked.connect(partial(self.update_stocks, -1, stock))
-			row.addWidget(self.sell_button)
+            # Sell Button
+            self.sell_button = utils.create_button("Sell")
+            self.sell_button.clicked.connect(partial(self.update_stocks, -1, stock))
+            row.addWidget(self.sell_button)
 
-			# Stocks Owned
-			row.addWidget(self.stocks_owned)
-			self.table.addLayout(row)
-			self.table.addWidget(utils.create_line())
+            # Stocks Owned
+            row.addWidget(self.stocks_owned)
+            self.table.addLayout(row)
+            self.table.addWidget(utils.create_line())
 
-		# Setup Grid
-		layout = QGridLayout()
+        # Setup Grid
+        layout = QGridLayout()
 
-		# Titles
-		value = format(round(sum(stock.equity for stock in stocks), 2))
-		self.portfolio_value = utils.create_label(f"Portfolio Value: {value}")
-		self.portfolio_value.setObjectName("heading")
-		layout.addWidget(self.portfolio_value, 0, 1, 1, 1)
-		value = format(round(sum(stock.daily_return for stock in stocks), 2))
-		self.overall_return = utils.create_label(f"Today's Return: {value}")
-		utils.colour_heading(self.overall_return)
-		layout.addWidget(self.overall_return, 1, 1, 1, 1)
+        # Titles
+        value = utils.format(round(sum(stock.equity for stock in self.stocks), 2))
+        self.portfolio_value = utils.create_label(f"Portfolio Value: {value}")
+        self.portfolio_value.setObjectName("heading")
+        layout.addWidget(self.portfolio_value, 0, 1, 1, 1)
+        value = utils.format(round(sum(stock.daily_return for stock in self.stocks), 2))
+        self.overall_return = utils.create_label(f"Today's Return: {value}")
+        utils.colour_heading(self.overall_return)
+        layout.addWidget(self.overall_return, 1, 1, 1, 1)
 
-		# Scrollable Table
-		self.widget.setLayout(self.table)
-		self.scroll.setWidgetResizable(True)
-		self.scroll.setWidget(self.widget)
-		self.scroll.setFrameShape(QFrame.NoFrame)
-		layout.addWidget(self.scroll, 0, 3, 3, 1)
+        # Scrollable Table
+        self.widget.setLayout(self.table)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+        self.scroll.setFrameShape(QFrame.NoFrame)
+        layout.addWidget(self.scroll, 0, 3, 3, 1)
 
-		# Graph
-		layout.addWidget(self.canvas, 2, 0, 1, 3)
-		self.plot_graph((stocks[0].create_graph()[0], sum(stock.create_graph()[1] for stock in stocks)))
+        # Graph
+        layout.addWidget(self.canvas, 2, 0, 1, 3)
+        self.plot_graph((self.stocks[0].create_graph()[0], sum(stock.create_graph()[1] for stock in self.stocks)))
 
-		# Window Properties
-		self.setWindowTitle('Stock Trading Simulator')
-		self.setWindowIcon(QIcon("styles/icon.jpg"))
-		self.setGeometry(10, 10, 2560, 1440)
-		self.setMinimumSize(1500, 900)
-		self.setLayout(layout)
-		self.show()
+        # Window Properties
+        self.setWindowTitle("Stock Trading Simulator")
+        self.setWindowIcon(QIcon("styles/icon.jpg"))
+        self.setGeometry(10, 10, 2560, 1440)
+        self.setMinimumSize(1500, 900)
+        self.setLayout(layout)
+        self.show()
 
-	def update_graph(self, stock):
-		self.plot_graph(stock.create_graph())
-		self.update()
+    def update_graph(self, stock):
+        self.plot_graph(stock.create_graph())
+        self.update()
 
-	def update_stocks(self, value, stock):
-		stock.number_owned += value
+    def update_stocks(self, value, stock):
+        stock.number_owned += value
+
+        """
+		# Convert data to dictionary
+		data = dict()
+		for stock in self.stocks:
+			data.update({stock.ticker: stock.number_owned})
 
 		# Save stocks information to file
 		with open("stock_record.json") as f:
-			data = json.load(f)
-			data[stock.ticker] = stock.number_owned
+			json.dump(data, f)
 
 		self.update()
+		"""
 
-	def plot_graph(self, data):
-		# Setup figure to plot graph
-		self.figure.clear()
-		graph = self.figure.add_subplot(111)
+    def plot_graph(self, data):
+        # Setup figure to plot graph
+        self.figure.clear()
+        graph = self.figure.add_subplot(111)
 
-		# Hide x-axis and recolour y-axis text and ticks
-		graph.xaxis.set_visible(False)
-		graph.yaxis.label.set_color('#2da9b9')
-		graph.tick_params(axis='y', colors='#2da9b9')
+        # Hide x-axis and recolour y-axis text and ticks
+        graph.xaxis.set_visible(False)
+        graph.yaxis.label.set_color("#2da9b9")
+        graph.tick_params(axis="y", colors="#2da9b9")
 
-		# Recolour and resize graph borders
-		for axis in ['top', 'bottom', 'left', 'right']:
-			graph.spines[axis].set_color('#2da9b9')
-			graph.spines[axis].set_linewidth(3)
+        # Recolour and resize graph borders
+        for axis in ["top", "bottom", "left", "right"]:
+            graph.spines[axis].set_color("#2da9b9")
+            graph.spines[axis].set_linewidth(3)
 
-		# Plot data, using a coloured line
-		graph.plot(data[0], data[1], color='#2da9b9', linewidth=2, marker='.', linestyle='-')
+        # Plot data, using a coloured line
+        graph.plot(data[0], data[1], color="#2da9b9", linewidth=2, marker=".", linestyle="-")
 
-		# Hover animation
-		def show_annotation(sel):
-			_, y = sel.target
-			sel.annotation.set_text(f"{y:.3f}")
+        # Hover animation
+        def show_annotation(sel):
+            _, y = sel.target
+            sel.annotation.set_text(f"{y:.2f}")
 
-		cursor = mplcursors.cursor(graph, hover=True)
-		cursor.connect('add', show_annotation)
+        cursor = mplcursors.cursor(graph, hover=True)
+        cursor.connect("add", show_annotation)
 
-		self.canvas.draw()
+        self.canvas.draw()
 
 
-if __name__ == '__main__':
-	app = QApplication(sys.argv)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-	# Use QSS style sheets
-	app.setStyleSheet(open("styles/styles.qss").read())
+    # Use QSS style sheets
+    app.setStyleSheet(open("styles/styles.qss").read())
 
-	ex = Window()
-	sys.exit(app.exec_())
+    ex = Window()
+    sys.exit(app.exec_())
